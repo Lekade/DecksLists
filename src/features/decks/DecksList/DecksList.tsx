ebc45f5dd2251cffdf7e67de983e73e3b32bb138
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-08 16:12:31 +0300

    getting decks and rendering them

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,23 @@
 import s from './DecksList.module.css'
+import {useEffect} from "react";
+import {decksAPI} from "../decks-api.ts";
+import {useAppDispatch, useAppSelector} from "../../../app/store.ts";
+import {setDecksAC} from "../decks-reducer.ts";
+import {DeckItem} from "./DeckItem/DeckItem.tsx";
+import {selectDecks} from "../decks-selectors.ts";
 
 export const DecksList = () => {
-  return <ul className={s.list}></ul>
+
+    const dispatch = useAppDispatch()
+    const decks = useAppSelector(selectDecks)
+
+    useEffect(() => {
+        decksAPI.fetchDecks().then((res) => {
+            dispatch(setDecksAC(res.data.items))
+        })
+    }, [])
+
+    return <ul className={s.list}>
+        {decks.map(deck => <DeckItem key={deck.id} deck={deck}/>)}
+    </ul>
 }
