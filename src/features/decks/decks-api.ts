0f9276934a0921106d171322d501a909d7add941
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-08 16:04:00 +0300

    created a decksAPI object and used the fetchDecks method in it to get decks (created response types)

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,40 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+
+export const decksAPI ={
+  fetchDecks(){
+    return instance.get<FetchDecksResponseType>('/v2/decks')
+  }
+}
+
+type FetchDecksResponseType = {
+  items: Decks[],
+  pagination: Pagination
+}
+
+export type Decks = {
+  author: Author,
+  isFavorite: boolean,
+  id: string,
+  userId: string,
+  name: string,
+  isPrivate: boolean,
+  cover: string,
+  created: string,
+  updated: string,
+  cardsCount: number,
+}
+
+type Author = {
+  id: string,
+  name: string,
+}
+
+type Pagination = {
+  currentPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  totalItems: number;
+}
